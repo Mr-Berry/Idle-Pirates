a114db2c3780f245496d7508d996b5f2e1b52585
Jason Berry <mrberry1990@gmail.com>
2018-02-24 10:39:54 -0500

    added enemy gold scripts, added player script and edited game manager to make it an instance

---
+++ Assets/Script/PirateBooty.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateBooty : MonoBehaviour {
+
+	public int m_goldAward;
+
+	public void AwardGold() {
+		PlayerShip.Instance.m_pirateBooty += m_goldAward;
+	}
+
+	public void SetGold(int gold) {
+		m_goldAward += gold;
+	}
+}
