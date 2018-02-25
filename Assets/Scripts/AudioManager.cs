5ecf6784b089b2a844dd54a288f9f213b0f5d79e
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-02-25 10:44:42 -0500

    added audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip CannonFire_V1;
+	public AudioClip CannonFire_V2;
+	public AudioClip CannonFire_V3;
+	public AudioClip CannonFire_V4;
+
+	public AudioClip CannonHit_V1;
+	public AudioClip CannonHit_V2;
+	public AudioClip CannonHit_V3;
+
+	private AudioSource speaker;
+
+	public static AudioManager Instance { get { return m_instance; }}
+
+	private static AudioManager m_instance;
+
+	void Awake() {
+		m_instance = this;
+	}
+
+	void Start() {
+		speaker = GetComponent<AudioSource>();
+	}
+
+	public float SetRandomPitch(float low, float high) {
+		float pitch = Random.Range(low, high);
+		return pitch;
+	}
+
+	public void PlayRandom_CannonFire() {
+		int index = Random.Range(0, 5);
+		switch(index) {
+			case 1: 
+				mixer(CannonFire_V1, SetRandomPitch(0.5f, 1.5f));
+				break;
+			case 2:
+				mixer(CannonFire_V2, SetRandomPitch(0.5f, 1.5f));
+				break;
+			case 3: 
+				mixer(CannonFire_V3, SetRandomPitch(0.5f, 1.5f));
+				break;
+			case 4:
+				mixer(CannonFire_V4, SetRandomPitch(0.5f, 1.5f));
+				break; 
+		}
+	}
+
+	public void mixer(AudioClip audio, float pitch) {
+		speaker.pitch = pitch;
+		speaker.clip = audio;
+		speaker.Play();
+	}
+}
